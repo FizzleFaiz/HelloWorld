845934c7e744e133b181db9e1560640418b29956
FizzleFaiz <39010192+FizzleFaiz@users.noreply.github.com>
2020-01-13 13:11:29 +0800

    add comment

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//This is a comment
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
